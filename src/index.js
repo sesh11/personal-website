df2cee57607e853802825a8487f51eaaabb6b9d6
Nithin Seshadri <seshn@mac.lan>
2025-01-04 15:04:27 -0500

    removed amplify provider in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { AmplifyProvider } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -13,9 +12,7 @@ Amplify.configure(awsconfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AmplifyProvider>
       <App />
-    </AmplifyProvider>
   </React.StrictMode>
 );
 
